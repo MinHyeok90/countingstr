011c303e2f8c92a5bff8c5a0487923fe7cce2682
likelion_minhyeok <dufflaa@gmail.com>
2016-05-10 18:55:50 +0000

    blink input error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,16 @@ class HomeController < ApplicationController
   end
   
   def write
-    newpost = Post.create(title: params[:title],content: params[:content])
-    newpost.save
-    redirect_to :back
+    newpost = Post.new(title: params[:title],content: params[:content])
+    if newpost.save
+      redirect_to :back
+    else
+      render :text => newpost.errors.messages[:title].first
+    end
   end
   
   def reply_write
-    new_reply = Reply.create(content: params[:content],post_id: params[:post_id])
+    new_reply = Reply.new(content: params[:content],post_id: params[:post_id])
     new_reply.save
     redirect_to :back
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
     has_many :replies
+    validates :title, presence: { messages: "제목이 비었습니다!"}
 end
